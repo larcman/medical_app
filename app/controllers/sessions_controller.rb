c10bf48152a83ae59e1e52fe444977f2d73f58e5
Luis Reyes <larcman@gmail.com>
2017-02-08 22:11:34 -0600

    Route: Log In as root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       redirect_to 'login'
     end
   end
-  
+
   def destroy 
     session[:user_id] = nil 
     redirect_to '/' 
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # SOME EXAMPLES
-  #root 'albums#index'
-  #get 'albums' => 'albums#index'
-  #get 'albums/new' => 'albums#new'
-  #get 'albums/:id' => 'albums#show', as: :album
-  #post 'albums' => 'albums#create'
+  # root 'albums#index'
+  # get 'albums' => 'albums#index'
+  # get 'albums/new' => 'albums#new'
+  # get 'albums/:id' => 'albums#show', as: :album
+  # post 'albums' => 'albums#create'
   
-  #root 'sessions#new'
-  get 'signup'  => 'users#new'
-  resources :users
+  root 'sessions#new'
+  # get 'signup'  => 'users#new'
+  # resources :users
   get 'login'  => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'  
