3ddff2cd105d5cb6a027433987d160ce6fc97569
Luis Reyes <larcman@gmail.com>
2017-02-12 00:40:09 -0600

    Added default records migration file

---
+++ db/migrate/20170212062933_add_default_records.rb
@@ -0,0 +1,27 @@
+class AddDefaultRecords < ActiveRecord::Migration[5.0]
+  def change
+    reversible do |dir|
+      dir.up do
+        Center.create id: '0',
+                      code: 'CUCS',
+                      name: 'Centro Universitario de Ciencias de la Salud UDG'
+        
+        User.create id: '0',
+                    firstname: 'Admin',
+                    lastname: 'Admin',
+                    email: 'larcman@gmail.com',
+                    username: 'admin',
+                    isadmin: 't',
+                    center_id: '0',
+                    password_digest: User.digest('@dm!n')
+                    
+        Research.create id: '0',
+                        code: 'MI INVESTIGACION',
+                        name: 'Algun nombre para esta investigacion',
+                        description: 'Alguna descripcion para esta investigacion',
+                        user_id: '0',
+                        center_id: '0'
+      end
+    end
+  end
+end
