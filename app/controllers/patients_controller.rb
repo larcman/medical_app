7cf2cf6b304869da309194d149a847f59ef3f5a3
Luis Reyes <larcman@gmail.com>
2017-03-17 00:08:22 -0600

    Added unique code constraint for Patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -18,7 +18,7 @@ class PatientsController < ApplicationController
       @patient.save!
       flash[:success] = "Paciente añadido al protocolo satisfactoriamente!"
     rescue ActiveRecord::RecordNotUnique
-      flash[:error] = "Error! El codigo elegido ya esta en uso."
+      flash[:error] = "Error! El codigo elegido ya esta en uso o este Candidato ya es paciente del protocolo."
     rescue
       flash[:error] = "Error! Asegurese de llenar todos los campos correctamente."
     end
@@ -48,7 +48,7 @@ class PatientsController < ApplicationController
       @patient.update!(patient_params)
       flash[:success] = "Paciente actualizado satisfactoriamente!"
     rescue ActiveRecord::RecordNotUnique
-      flash[:error] = "Error! El codigo elegido ya esta en uso."
+      flash[:error] = "Error! El codigo elegido ya esta en uso o este Candidato ya es paciente del protocolo."
     rescue
       flash[:error] = "Error! Asegurese de llenar todos los campos correctamente."
     end
